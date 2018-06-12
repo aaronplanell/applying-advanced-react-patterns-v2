823166d55054fd2e35ed8c7057baf7d4b6a37283
Aaron Planell <aaronplanell@gmail.com>
2018-06-12 14:03:46 +0200

    State Reducer Component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,28 @@ import './App.css';
 import Toggle from './Toggle';
 
 class App extends Component {
+  initialState = { timesClicked: 0 };
+  state = this.initialState;
+
+  handleToggle = (...args) => {
+    this.setState(({ timesClicked }) => {
+      return { timesClicked: timesClicked + 1 };
+    });
+    console.log('onToggle', ...args);
+  };
+
+  handleReset = (...args) => {
+    this.setState(this.initialState);
+    console.log('onReset', ...args);
+  };
+
+  stateReducer = (state, changes) => {
+    if (this.state.timesClicked >= 4) {
+      return { ...changes, on: false };
+    }
+    return changes;
+  };
+
   render() {
     return (
       <div className="App">
@@ -11,7 +33,11 @@ class App extends Component {
           <h1 className="App-title">Applying Advanced React Patters v2</h1>
         </header>
         <div className="App-intro">
-          <Toggle onToggle={(...args) => console.log('onToggle', ...args)} />
+          <Toggle
+            onToggle={this.handleToggle}
+            onReset={this.handleReset}
+            stateReducer={this.stateReducer}
+          />
         </div>
       </div>
     );
--- src/Toggle.js
+++ src/Toggle.js
@@ -2,12 +2,27 @@ import React from 'react';
 import { Switch } from './Switch';
 
 class Toggle extends React.Component {
-  static defaultProps = { initialOn: false };
+  static defaultProps = {
+    initialOn: false,
+    stateReducer: (state, changes) => changes,
+  };
   initialState = { on: this.props.initialOn };
   state = this.initialState;
 
+  internalSetState(changes, callback) {
+    this.setState(currentState => {
+      // As setState, internalSetState can reveive a function or a state as a first argument
+      const changesObject =
+        typeof changes === 'function' ? changes(currentState) : changes;
+      const reducerObject =
+        this.props.stateReducer(currentState, changesObject) || {};
+      // Only returns reducerObject if has changes to avoid an unecessary renders
+      return Object.keys(reducerObject).length > 0 ? reducerObject : null;
+    }, callback);
+  }
+
   toggle = () =>
-    this.setState(
+    this.internalSetState(
       ({ on }) => ({ on: !on }),
       () => {
         this.props.onToggle(this.state.on);
@@ -15,7 +30,12 @@ class Toggle extends React.Component {
     );
 
   reset = () => {
-    this.setState(this.initialState);
+    this.internalSetState(
+      () => this.initialState,
+      () => {
+        this.props.onReset(this.state.on);
+      }
+    );
   };
 
   render() {
