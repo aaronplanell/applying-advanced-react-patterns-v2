129da449d65b464215f459114a4225aa652bd939
Aaron Planell Lopez <aplanell@meteo.cat>
2018-06-22 17:14:14 +0200

    Control Props Component

--- src/App.js
+++ src/App.js
@@ -4,14 +4,23 @@ import './App.css';
 import Toggle from './Toggle';
 
 class App extends Component {
+  state = { on: false };
+
+  onToggle = on => {
+    this.setState({ on });
+  };
+
   render() {
+    const { on } = this.state;
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Applying Advanced React Patters v2</h1>
         </header>
         <p className="App-intro">
-          <Toggle onToggle={() => console.log('onToggle')} />
+          <Toggle onToggle={this.onToggle} on={on} />
+          <br />
+          <Toggle onToggle={this.onToggle} on={on} />
         </p>
       </div>
     );
--- src/Toggle.js
+++ src/Toggle.js
@@ -3,15 +3,32 @@ import { Switch } from './Switch';
 
 class Toggle extends React.Component {
   state = { on: false };
-  toggle = () =>
-    this.setState(
-      ({ on }) => ({ on: !on }),
-      () => {
-        this.props.onToggle(this.state.on);
-      }
-    );
+
+  isControlled = prop => {
+    return this.props[prop] !== undefined;
+  };
+
+  getState = () => {
+    return {
+      on: this.isControlled('on') ? this.props.on : this.state.on,
+    };
+  };
+
+  toggle = () => {
+    if (this.isControlled('on')) {
+      this.props.onToggle(!this.getState().on);
+    } else {
+      this.setState(
+        ({ on }) => ({ on: !on }),
+        () => {
+          this.props.onToggle(this.getState().on);
+        }
+      );
+    }
+  };
+
   render() {
-    const { on } = this.state;
+    const { on } = this.getState();
     return <Switch on={on} onClick={this.toggle} />;
   }
 }
